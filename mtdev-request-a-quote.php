9330bf054dd0b589114273db682a875b7270c30b
Marta Torre <hola@martatorre.dev>
2024-12-14 10:19:07 +0100

    Create mtdev-request-a-quote.php

---
+++ mtdev-request-a-quote.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: MTDev Request a Quote for WooCommerce
+Plugin URI: https://github.com/martatorredev/mtdev-request-a-quote-for-woocommerce
+Description: A custom WooCommerce plugin by Marta Torre to hide prices and allow customizable buttons for specific products.
+Version: 1.1
+Author: Marta Torre
+Author URI: https://martatorre.dev
+License: GPLv2 or later
+Text Domain: mtdev-request-a-quote
+Domain Path: /languages
+*/
